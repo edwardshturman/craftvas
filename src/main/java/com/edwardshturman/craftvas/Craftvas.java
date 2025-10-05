76d3a622177c41838df5d4b3c6d9ab289aaf78b2
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2025-10-04 23:49:50 -0700

    Add Hello World

---
+++ src/main/java/com/edwardshturman/craftvas/Craftvas.java
@@ -0,0 +1,29 @@
+package com.edwardshturman.craftvas;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Craftvas extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage(ChatColor.GOLD + "Hi from Craftvas!");
+    }
+}
